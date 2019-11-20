4f042f7fc6ca5cdeca847e962b48117bff30a503
caoxinghua3 <caoxinghua3@jd.com>
2019-11-20 15:48:48 +0800

    适应新的api

--- BBS/src/main/java/com/hua/module/bbs/BBSActivity.java
+++ BBS/src/main/java/com/hua/module/bbs/BBSActivity.java
@@ -1,7 +1,6 @@
 package com.hua.module.bbs;
 
 import android.app.Activity;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
